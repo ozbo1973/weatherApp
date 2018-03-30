bfcb48ff20b8c8775f538a1e9aab07c3319c1f73
ozbo1973 <bbbove20@gmail.com>
2018-03-30 07:12:32 -0600

    changing accessing keys.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from 'axios'
+import keys from '../config';
 
-const API_KEY = '2c62ddab2daa55809a34564257c0e272';
+const API_KEY = keys.API_KEY
+console.log(API_KEY);
+// if (process.env.ENVIRONMENT === 'production'){API_KEY=P_API_KEY}
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+//Varibales
+import devKey from './devKey'
+
+module.exports = {
+  API_KEY : devKey.API_KEY,
+  P_API_KEY : API_KEY
+}
